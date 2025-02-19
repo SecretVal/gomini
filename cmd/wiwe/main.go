df7a030933441313b2e78f9486fe49f3bd8a1fd4
secretval <108518296+SecretVal@users.noreply.github.com>
2025-02-19 14:54:51 +0100

    feat: gui

--- cmd/wiwe/main.go
+++ cmd/wiwe/main.go
@@ -3,8 +3,16 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
+	"image/color"
+	"log"
 	"os"
 	"strings"
+
+	"gioui.org/app"
+	"gioui.org/font/gofont"
+	"gioui.org/op"
+	"gioui.org/text"
+	"gioui.org/widget/material"
 )
 
 var TLSConfig = tls.Config{
@@ -59,5 +67,45 @@ func main() {
 	}
 	url := args[0]
 
-	fmt.Println(make_gemini_query(url))
+	buf := make_gemini_query(url)
+	log.Printf("Received %d bytes", len(buf))
+
+	go func() {
+		window := new(app.Window)
+		err := render_window(window, buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}()
+	app.Main()
+}
+
+func render_window(window *app.Window, str string) error {
+	th := material.NewTheme()
+	th.Shaper = text.NewShaper(text.WithCollection(gofont.Collection()))
+	th.Bg = color.NRGBA{R: 18, G: 18, B: 18, A: 255}
+	var ops op.Ops
+	for {
+		switch e := window.Event().(type) {
+		case app.DestroyEvent:
+			return e.Err
+		case app.FrameEvent:
+			// This graphics context is used for managing the rendering state.
+			gtx := app.NewContext(&ops, e)
+
+			// Define an large label with an appropriate text:
+			body := material.Label(th, 16, str)
+
+			// Change the color of the label.
+			black := color.NRGBA{R: 0, G: 0, B: 0, A: 255}
+			body.Color = black
+
+			// Draw the label to the graphics context.
+			body.Layout(gtx)
+
+			// Pass the drawing operations to the GPU.
+			e.Frame(gtx.Ops)
+		}
+	}
 }
